891b15898bc6ed74f0d3c70f58c7556ca48d4bc2
Jo El <joelag1235@gmail.com>
2023-06-08 17:42:06 +0700

    Almost updated

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import pandas as pd
 import os
 
 # EDA
-import ydata_profiling
+# from ydata_profiling import profile_report
+import pyarrow.feather as feather
+from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 # ML
@@ -14,7 +16,7 @@ from pycaret.classification import setup, compare_models, pull, save_model
 
 
 with st.sidebar:
-    st.image('https://github.com/joelsiby02/Auto-ML-WebApp/blob/1c7e3c821d7e98cd3f63fbbd5ea59ef30c3b2085/Logo.jpg') # Image for sidebar
+    st.image('Logo.jpg') # Image for sidebar
     st.title('Auto ML App') # title for SideBar
     choice = st.radio("User Tools",['Upload', 'Profiling', 'ML', "Download"] ) # Controling navigations
     st.info("This application allows you to built an Automated ML pipeline using Streamlit, Pandas Profiling & PyCaret") # Info to describe
@@ -30,23 +32,23 @@ if choice == "Upload":
     file = st.file_uploader("Upload your Data Set here")
     
     if file:
-        df = pd.read_csv(file, index_col = None)  # reading the DataSet 
-        df.to_csv("SourceData.csv", index = None)
-        st.dataframe(df) # Render the dataFrame 
+        df = pd.read_csv(file, index_col=None, encoding='latin1')  # Specify the appropriate encoding
+        feather.write_feather(df, "SourceData.feather")  # Save DataFrame to Feather format
+        st.dataframe(df)  # Render the DataFrame
+ 
 
 
 if choice == "Profiling":
     st.title("Exploratory Data Analysis Report")
-    profile_report = df.profile_report()
-    st_profile_report(profile_report)
-    pass
+    profile = ProfileReport(df)
+    st_profile_report(profile)
 
 
 if choice == 'ML':
     st.title("Machine Learning on Progress")
     target = st.selectbox("Select Your Target", df.columns)
     if st.button("Train Model"):
-        setup(df, target = target, silent = True)
+        setup(df, target = target) #  silent = True
         setup_df = pull()
         st.info("Experementing ML Settings")
         st.dataframe(setup_df)
@@ -54,7 +56,7 @@ if choice == 'ML':
         # Selecting models using compare_model & pull using pycaret fn's
         best_model = compare_models()
         compare_df = pull()
-        st.info("This is the ML Models")
+        st.info("These are the trained Models")
         st.dataframe(compare_df)
         best_model
         
@@ -65,4 +67,4 @@ if choice == 'ML':
 if choice == 'Download':
     with open("Best_Model.pkl", 'rb') as f:
         st.download_button("Download the Model", f, "Trained_Model.pkl")
-    pass
+        
